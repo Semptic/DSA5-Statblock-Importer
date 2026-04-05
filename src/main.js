944048c60f3c237834f0e446051a770ab3332fde
Stefan Kuhn <stefan@kuhn.cloud>
2026-04-05 11:28:14 +0200

    feat: add module scaffold and sidebar button stub

--- src/main.js
+++ src/main.js
@@ -1,1 +1,11 @@
-// placeholder
+Hooks.on('renderActorDirectory', (app, html) => {
+  if (!game.user.isGM) return
+  const button = $(`<button type="button">
+    <i class="fas fa-file-import"></i>
+    ${game.i18n.localize('DSA5SI.button.import')}
+  </button>`)
+  html.find('.directory-header .action-buttons').append(button)
+  button.on('click', () => {
+    ui.notifications.info('DSA5 Statblock Importer: coming soon')
+  })
+})
