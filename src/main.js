6a443e1224ecfe85f2cfccc22a6dba73258cdda4
Stefan Kuhn <stefan@kuhn.cloud>
2026-04-06 09:13:49 +0200

    fix: use native DOM in renderActorDirectory hook (Foundry v13 passes HTMLElement not jQuery)

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import { ImportDialog } from './import-dialog.js'
 
 Hooks.on('renderActorDirectory', (app, html) => {
   if (!game.user.isGM) return
-  const button = $(`<button type="button">
-    <i class="fas fa-file-import"></i>
-    ${game.i18n.localize('DSA5SI.button.import')}
-  </button>`)
-  html.find('.directory-header .action-buttons').append(button)
-  button.on('click', () => new ImportDialog().render(true))
+  // Foundry v13 passes HTMLElement; use native DOM
+  const root = html instanceof HTMLElement ? html : html[0]
+  const actionButtons = root?.querySelector('.directory-header .action-buttons')
+  if (!actionButtons) return
+  const button = document.createElement('button')
+  button.type = 'button'
+  button.innerHTML = `<i class="fas fa-file-import"></i> ${game.i18n.localize('DSA5SI.button.import')}`
+  actionButtons.append(button)
+  button.addEventListener('click', () => new ImportDialog().render(true))
 })
